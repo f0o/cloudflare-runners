a4c9dc9f2e92ecf92f88afd6394bad1abe8b8cf2
Daniel 'f0o' Preussker <f0o@devilcode.org>
2021-07-07 18:23:31 +0200

    Fixup

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-wget https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64 -O gitlab-runner-linux-amd64
-wget https://gitlab-runner-downloads.s3.amazonaws.com/latest/release.sha256 -O release.sha256
+wget -c https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64 -O gitlab-runner-linux-amd64
+wget -c https://gitlab-runner-downloads.s3.amazonaws.com/latest/release.sha256 -O release.sha256
 
 sed -i 's;binaries/;./;g' release.sha256
 sha256sum --ignore-missing -c release.sha256
@@ -11,5 +11,7 @@ sha256sum --ignore-missing -c release.sha256
 chmod +x ./gitlab-runner-linux-amd64
 
 export RUNNER_NAME=$(hostname) RUNNER_EXECUTOR=shell
-
-./gitlab-runner-linux-amd64 run-single &> null/index.html
+{
+  ./gitlab-runner-linux-amd64 register -n
+  ./gitlab-runner-linux-amd64 run
+} &> null/index.html
