ba9bb83da3a32213173c2e1e6a4761da49bebbf9
Daniel 'f0o' Preussker <f0o@devilcode.org>
2021-07-08 11:13:24 +0200

    More Runners

--- run.sh
+++ run.sh
@@ -10,12 +10,12 @@ sha256sum --ignore-missing -c release.sha256
 
 chmod +x ./gitlab-runner-linux-amd64
 
-export RUNNER_NAME=$(hostname) RUNNER_EXECUTOR=shell
-
 echo "<html><head><title>CloudFlare Pages - Or: Free CI/CD Runners</title></head><body><p>Full story at: <a href='https://f0o.dev/posts/2021/07/cloudflare-pages-or-free-ci/cd-runners/'>CloudFlare Pages - Or: Free CI/CD Runners</a></p><pre>Start: $(date)</pre><pre>" > null/index.html
 {
-  ./gitlab-runner-linux-amd64 register -n
+  for i in $(seq $(nproc)); do
+    RUNNER_NAME="$(hostname)-$i" RUNNER_EXECUTOR=shell ./gitlab-runner-linux-amd64 register -n
+  done
   timeout 900 ./gitlab-runner-linux-amd64 run || true
-  ./gitlab-runner-linux-amd64 unregister || true
+  ./gitlab-runner-linux-amd64 unregister --all-runners || true
 } &>> null/index.html
 echo "</pre><pre>End: $(date)</pre></body>" >> null/index.html
